cfd41d613b00d871467d9a65305c0a7afeba14a5
Istabraq <angel.ofmoons@gmail.com>
2017-07-16 01:35:03 -0400

    Validate name and include it in the message with the required format.

--- index.php
+++ index.php
@@ -18,7 +18,25 @@ switch ($action) {
          * validate and process the name
          ************************************************/
         // 1. make sure the user enters a name
+	if(empty($name)) {
+	  $message = "Please provide a name.";
+	  break;
+	}
         // 2. display the name with only the first letter capitalized
+	$name = strtolower($name);
+	$name = ucwords($name);
+	$space_index = strpos($name, ' ');
+	if ($space_index === false) {
+	  $first_name = $name;
+	} else {
+	  // Get everything in name before the space
+	  $first_name = substr($name, 0, $space_index); 
+	}
+
+	// 3. Add the name to the message
+	$message = "Hello $first_name,\n\n";
+	$message .= "Thank you for entering this data:\n\n";
+	$message .= "Name: $name\n";
 
         /*************************************************
          * validate and process the email address
@@ -31,12 +49,6 @@ switch ($action) {
          ************************************************/
         // 1. make sure the user enters at least seven digits, not including formatting characters
         // 2. format the phone number like this 123-4567 or this 123-456-7890
-
-        /*************************************************
-         * Display the validation message
-         ************************************************/
-        $message = "This page is under construction.\n" .
-                   "Please write the code that process the data.";
 
         break;
 }
